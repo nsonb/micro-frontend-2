59b73b73c7d5c3bab5c9c136a3523708ae9d835e
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-22 23:37:11 +0200

    experimenting

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
-  // modifying workflow
+  // modifying
   return <div>
     <MarketingApp></MarketingApp>
   </div>
