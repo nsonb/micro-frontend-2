ac14add3a553afba760e396ec0101bdcc77b71cc
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-23 00:27:36 +0200

    getting s3 to deploy

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
-  // pls run :)
+  // pls run :) final 
   return <div>
     <MarketingApp></MarketingApp>
   </div>
