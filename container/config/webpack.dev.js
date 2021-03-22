d6951f8f161172318986aaee996b1b932feae938
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-22 17:45:53 +0200

    shared package

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
 
 const commonConfig = require('./webpack.common')
 const devConfig = {
@@ -20,7 +21,8 @@ const devConfig = {
       name: 'container',
       remotes: {
         marketing : 'marketing@http://localhost:8081/remoteEntry.js'
-      }
+      },
+      shared: packageJson.dependencies
     })
   ]
 }
--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
 
 const commonConfig = require('./webpack.common')
 const devConfig = {
@@ -21,7 +22,8 @@ const devConfig = {
       filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp' : './src/bootstrap'
-      }
+      },
+      shared: packageJson.dependencies
     })
   ]
 }
