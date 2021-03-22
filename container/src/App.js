3d09384df9394194e965b492017720f2124525f7
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-23 00:28:47 +0200

    fix minor error in workflow

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
-  // pls run :) final 
+  // pls run :) final :v
   return <div>
     <MarketingApp></MarketingApp>
   </div>
