3bd845caa2e4caada79b453eab7a4bf46b4849c1
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-23 00:17:41 +0200

    changing container.yml

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
-  // pls run
+  // pls run :)
   return <div>
     <MarketingApp></MarketingApp>
   </div>
