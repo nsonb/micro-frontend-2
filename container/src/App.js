3b9e30a6ddde255104420fb463a00c60ac053040
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-22 22:06:41 +0200

    Update App.js

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
-  //testing
+  //testing things
   return <div>
     <MarketingApp></MarketingApp>
   </div>
