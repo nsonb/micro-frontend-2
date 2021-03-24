1f641b45cec1415816a69620f0d0ad00ec7bd54d
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-24 17:29:29 +0200

    updated webpack dev

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Switch, Route, Router } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 
+import Signin from './components/Signin'
+import Signup from './components/Signup'
+
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au'
@@ -13,7 +16,8 @@ export default ({history}) => {
      <StylesProvider generateClassName={generateClassName}>
        <Router history={history}>
         <Switch>
-          
+          <Route path='/auth/signin' component={Signin}/>
+          <Route path='/auth/signup' component={Signup}/>
         </Switch>
        </Router>
      </StylesProvider>
