23763f50239bcd69363857a0f08cb2e4c5977eaa
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-24 18:17:40 +0200

    connected auth app to container

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+import AuthApp from './components/AuthApp'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -16,6 +17,10 @@ export default () => {
       <BrowserRouter>
         <div>
           <Header/>
+          <Switch>
+            <Route path='/auth' component={AuthApp}/>
+            <Route path='/' component={MarketingApp}/>
+          </Switch>
           <MarketingApp/>
         </div>
       </BrowserRouter>
